1411b331d18f5dd6376d13f859a6d95e4c57b295
Ramachandra Reddy <ramachandra.reddy@kaiostech.com>
2020-12-24 12:39:22 +0530

    button stories error fix

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -5,10 +5,11 @@ type props = {
   style : object;
   label: string;
   onClick: any;
+  disabled: boolean;
 }
 
 const Button = (props: props) => (
-  <button onClick={props.onClick} style={props.style}>
+  <button onClick={props.onClick} style={props.style} disabled={props.disabled}>
     {props.label && <span>{props.label}</span>}
   </button>
 )
--- src/stories/buttonStories.tsx
+++ src/stories/buttonStories.tsx
@@ -4,16 +4,16 @@ import { storiesOf } from '@storybook/react';
 import Button from '../components/atoms/button';
 
 storiesOf('Button', module)
-  .add('with primary', () => <Button label="Primary Button" />)
-  .add('with outline', () => <Button
-    label="Ouline Button"
+  .add('with primary', () => <Button disabled={false} style={{}} onClick="" label="Primary Button" />)
+  .add('with outline', () => <Button disabled={false} onClick=""
+    label="Ouline Button" 
     style={{ background: 'transparent', border: '3px solid #fecd43' }}
   />)
-  .add('with rounder corners', () => <Button
+  .add('with rounder corners', () => <Button  onClick=""  disabled={false}
     label="Rounded Button"
     style={{ borderRadius: '15px' }}
   />)
-  .add('disabled', () => <Button disabled
+  .add('disabled', () => <Button disabled={true} onClick=""
     label="Disabled Button"
     style={{ background: 'gray' , border: 'gray', cursor: 'not-allowed' }}
   />)
